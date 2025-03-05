ae519d4c436e62e2fc70e24d7406b6e6f52849c0
DragonTops <34971983+DragonTops@users.noreply.github.com>
2025-03-05 15:11:24 +0300

    Update roman_class.py

--- roman_class.py
+++ roman_class.py
@@ -90,10 +90,3 @@ class Roman:
         """Возвращает строковое представление в виде римского числа."""
         return self.__int_to_roman(self.__value)
 
-# Примеры использования
-a = Roman("XX")    # 20
-b = Roman("VII")   # 7
-print(a + a)       # XL (40)
-print(a - b)       # XIII (13)
-print(a / b)       # II (2)
-print(a * b)       # CXL (140)
